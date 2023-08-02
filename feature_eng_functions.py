e74d47404942d917076397e33fc4e087fd676706
Bruna Fistarol <bruna_fistarol@outlook.com>
2023-08-02 14:39:02 -0300

    python functions in one single file

--- feature_eng_functions.py
+++ feature_eng_functions.py
@@ -1,6 +1,7 @@
 import os
 from Bio import AlignIO
 from Bio.Align.Applications import MafftCommandline
+import subprocess
 
 def read_fasta_file(file_path):
     sequences = {}
@@ -36,12 +37,12 @@ def get_multifasta(datadir, output_directory):
 
     protein_families = {protein_families[i]: {} for i in range(len(protein_families))}
 
-    os.mkdir(os.path.join(output_directory, 'Pfam_mf'))
+    os.mkdir(os.path.join(output_directory, 'Pfam_multifasta'))
 
     for file_path in os.listdir(datadir):
         sequences = read_fasta_file(os.path.join(datadir, file_path))
         for header, sequence in sequences.items():
-            output_file_path = os.path.join(output_directory, f'Pfam_mf/{header}.fasta')
+            output_file_path = os.path.join(output_directory, f'Pfam_multifasta/{header}.fasta')
             write_fasta_file(output_file_path, file_path, sequence)
 
 #Multiple alignments with all multifasta files in a folder
@@ -54,3 +55,27 @@ def multiple_alignment(input_directory, output_directory):
         stdout, stderr = mafft_cline() 
         with open(output_file_path, 'w') as f:
             f.write(stdout)
+
+def run_hmmbuild(input_directory, output_directory, threads):
+    os.mkdir(os.path.join(output_directory, 'Pfam_hmm'))
+    for protein_family in os.listdir(input_directory):
+        input_file_path = input_directory + f'/{protein_family}'
+        protein_family = protein_family.split('.')[0]
+        output_file_path = os.path.join(output_directory, f'Pfam_hmm/{protein_family}.hmm')
+        command_line = (f'hmmbuild --cpu {threads} {output_file_path} {input_file_path}')
+        subprocess.run(command_line, shell=True)
+
+def align_hmm(path_specie):
+    path_multifasta = os.path.join(path_specie, 'Pfam_multifasta')
+    path_hmm = os.path.join(path_specie, 'Pfam_hmm')
+    output_directory = os.path.join(path_specie, 'Pfam_hmm_alig')
+    os.mkdir(output_directory)
+    for pfam in os.listdir(path_multifasta):
+        input_pfam_path = os.path.join(path_multifasta, pfam)
+        pfam = pfam.split('.')[0]
+        hmm = pfam + '.hmm'
+        input_hmm_path = os.path.join(path_hmm, hmm)
+        output_file_path = os.path.join(output_directory, f'{pfam}.aln')
+        command_line = (f'hmmalign -o {output_file_path} --outformat a2m {input_hmm_path} {input_pfam_path}')
+        subprocess.run(command_line, shell=True)
+
